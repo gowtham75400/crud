dbdb61c1c9588b8a6ff6010e532eb9deedfc620f
gowthamnk <gowthamnk67@gmail.com>
2024-08-10 10:17:32 +0530

    crudApp

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ mongoose.connect("mongodb://127.0.0.1/crud")
 
 app.post('/createUser',(req,res)=> {
     UserModel.create(req.body)
-    .then(users=>res.json(users))crud
+    .then(users=>res.json(users))
     .catch(err=>res.json(err))
 })
 
